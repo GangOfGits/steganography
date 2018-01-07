50c4deac2e75db25859b396f308300bb2323ed59
bench745 <35201139+bench745@users.noreply.github.com>
2018-01-07 20:44:14 +0000

    comments

--- hide.py
+++ hide.py
@@ -1,14 +1,15 @@
 from PIL import Image
 
-image = Image.open(input('name of PNG (.png) or bitmap (.bmp) >'))
+image = Image.open(input('name of PNG (.png) or bitmap (.bmp) >')) #akss for an image name and opens it 
 
-imageMod = image.mode
-imageSiz = image.size
+imageMod = image.mode #useless
+imageSiz = image.size #gets size of the image
 
-img = image.load()
+img = image.load() #img allows pixel editing
 
-msg = input('what would you like to hide >')
+msg = input('what would you like to hide >') #gets the text to hide
 
+##converts the string msg into a string of binary ascii values
 asciimsg = ''
 for c in msg:
     b = bin(ord(c))[2:]
@@ -17,21 +18,21 @@ for c in msg:
     asciimsg += b
 print(asciimsg)
 
-char = 0
+char = 0 #how far through asciimsg have we written
 y = 0
-while y <= imageSiz[1] and char < len(asciimsg):
+while y <= imageSiz[1] and char < len(asciimsg): #nested loopd iterate through pictures, conditions ensure we dont write more than the picture can hold
     x = 0
     while x <= imageSiz[0] and char < len(asciimsg):
-        if asciimsg[char] == '1':
-            if img[x,y][0] % 2 == 0:
-                prev = img[x,y]
+        if asciimsg[char] == '1': #if we a righting a 1
+            if img[x,y][0] % 2 == 0: #check if the red value is even, if the value is even it will end in zero so we must ajust by 1
+                prev = img[x,y] #this and next 4 for editing red value
                 if prev[0] > 1:
                     img[x,y] = (prev[0] - 1,prev[1],prev[2])
                 else:
                     img[x,y] = (prev[0] + 1,prev[1],prev[2])
             #print('1 :', str(x), str(y), bin(img[x,y][0])[-1:])
 
-        elif asciimsg[char] == '0':
+        elif asciimsg[char] == '0': #same as above only for zeros
             if img[x,y][0] % 2 != 0:
                 prev = img[x,y]
                 if prev[0] > 1:
@@ -45,4 +46,4 @@ while y <= imageSiz[1] and char < len(asciimsg):
 
     y += 1
 
-image.save('done.png')
+image.save('done.png') #save the image as done.png (poor practice to hard code i know)
